d915b74f5ad8fe3325cba8f8864b49c1e2b89fe1
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-27 21:40:24 +0530

    Created: Cab Invoice Generator and its Test File

---
+++ src/main/java/com/invoicegenerator/CabInvoiceGeneratorMain.java
@@ -0,0 +1,8 @@
+package com.invoicegenerator;
+
+public class CabInvoiceGeneratorMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Generator");
+    }
+}
