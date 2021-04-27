423b3f84103e495945f19844ea3770f394e58596
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-27 22:05:24 +0530

    ADDED: CalculateFare Method and its TestCase

--- src/test/java/com/invoicegenerator/TestCabInvoiceGenerator.java
+++ src/test/java/com/invoicegenerator/TestCabInvoiceGenerator.java
@@ -1,5 +1,17 @@
 package com.invoicegenerator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestCabInvoiceGenerator {
 
+    @Test
+    public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = cabInvoiceGenerator.calculateFare(distance,time);
+        System.out.println("Total Fare = " +totalFare);
+        Assertions.assertEquals(25, totalFare,0.0);
+    }
 }
